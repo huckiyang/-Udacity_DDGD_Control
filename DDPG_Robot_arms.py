74872b3b14ad37d415b73684317d72e9a566c471
huckiyang <huckiyang@gmail.com>
2019-06-13 08:32:06 +0800

    done

---
+++ DDPG_Robot_arms.py
@@ -0,0 +1,145 @@
+from ddpg_agent import Agent
+from collections import deque
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import time
+import torch
+from unityagents import UnityEnvironment
+
+%matplotlib inline
+
+
+## when running locally
+# env = UnityEnvironment(file_name='Reacher20.app')
+
+## when running via GCP
+env = UnityEnvironment(file_name='Reacher_Linux_NoVis/Reacher.x86_64')
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents
+num_agents = len(env_info.agents)
+print('Number of agents:', num_agents)
+
+# size of each action
+action_size = brain.vector_action_space_size
+print('Size of each action:', action_size)
+
+# examine the state space 
+states = env_info.vector_observations
+state_size = states.shape[1]
+print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+print('The state for the first agent looks like:', states[0])
+
+##################### Test Env with Random Work #####################################
+
+env_info = env.reset(train_mode=False)[brain_name]     # reset the environment    
+states = env_info.vector_observations                  # get the current state (for each agent)
+scores = np.zeros(num_agents)                          # initialize the score (for each agent)
+while True:
+    actions = np.random.randn(num_agents, action_size) # select an action (for each agent)
+    actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
+    env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+    next_states = env_info.vector_observations         # get next state (for each agent)
+    rewards = env_info.rewards                         # get reward (for each agent)
+    dones = env_info.local_done                        # see if episode finished
+    scores += env_info.rewards                         # update the score (for each agent)
+    states = next_states                               # roll over states to next time step
+    if np.any(dones):                                  # exit loop if episode finished
+        break
+print('Total score (averaged over agents) this episode: {}'.format(np.mean(scores)))
+
+# DDPG function
+
+def ddpg(n_episodes=500, max_t=1000, solved_score=30.0, consec_episodes=100, print_every=1, train_mode=True,
+         actor_path='actor_ckpt.pth', critic_path='critic_ckpt.pth'):
+    """Deep Deterministic Policy Gradient (DDPG)
+    
+    Params
+    ======
+        n_episodes (int)      : maximum number of training episodes
+        max_t (int)           : maximum number of timesteps per episode
+        train_mode (bool)     : if 'True' set environment to training mode
+        solved_score (float)  : min avg score over consecutive episodes
+        consec_episodes (int) : number of consecutive episodes used to calculate score
+        print_every (int)     : interval to display results
+        actor_path (str)      : directory to store actor network weights
+        critic_path (str)     : directory to store critic network weights
+
+    """
+    mean_scores = []                               # list of mean scores from each episode
+    min_scores = []                                # list of lowest scores from each episode
+    max_scores = []                                # list of highest scores from each episode
+    best_score = -np.inf
+    scores_window = deque(maxlen=consec_episodes)  # mean scores from most recent episodes
+    moving_avgs = []                               # list of moving averages
+    
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=train_mode)[brain_name] # reset environment
+        states = env_info.vector_observations                   # get current state for each agent      
+        scores = np.zeros(num_agents)                           # initialize score for each agent
+        agent.reset()
+        start_time = time.time()
+        for t in range(max_t):
+            actions = agent.act(states, add_noise=True)         # select an action
+            env_info = env.step(actions)[brain_name]            # send actions to environment
+            next_states = env_info.vector_observations          # get next state
+            rewards = env_info.rewards                          # get reward
+            dones = env_info.local_done                         # see if episode has finished
+            # save experience to replay buffer, perform learning step at defined interval
+            for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+                agent.step(state, action, reward, next_state, done, t)             
+            states = next_states
+            scores += rewards        
+            if np.any(dones):                                   # exit loop when episode ends
+                break
+
+        duration = time.time() - start_time
+        min_scores.append(np.min(scores))             # save lowest score for a single agent
+        max_scores.append(np.max(scores))             # save highest score for a single agent        
+        mean_scores.append(np.mean(scores))           # save mean score for the episode
+        scores_window.append(mean_scores[-1])         # save mean score to window
+        moving_avgs.append(np.mean(scores_window))    # save moving average
+                
+        if i_episode % print_every == 0:
+            print('\rEpisode {} ({} sec)  -- \tMin: {:.1f}\tMax: {:.1f}\tMean: {:.1f}\tMov. Avg: {:.1f}'.format(\
+                  i_episode, round(duration), min_scores[-1], max_scores[-1], mean_scores[-1], moving_avgs[-1]))
+        
+        if train_mode and mean_scores[-1] > best_score:
+            torch.save(agent.actor_local.state_dict(), actor_path)
+            torch.save(agent.critic_local.state_dict(), critic_path)
+                  
+        if moving_avgs[-1] >= solved_score and i_episode >= consec_episodes:
+            print('\nEnvironment SOLVED in {} episodes!\tMoving Average ={:.1f} over last {} episodes'.format(\
+                                    i_episode-consec_episodes, moving_avgs[-1], consec_episodes))            
+            if train_mode:
+                torch.save(agent.actor_local.state_dict(), actor_path)
+                torch.save(agent.critic_local.state_dict(), critic_path)  
+            break
+            
+    return mean_scores, moving_avgs
+
+
+# run the training loop
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=1)
+scores, avgs = ddpg()
+
+env.close()
+
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores, label='DDPG')
+plt.plot(np.arange(len(scores)), avgs, c='r', label='moving avg')
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.legend(loc='upper left');
+plt.savefig('Results/RobotArms'+"_Epis"+str(N_Epi)+"_"+time.strftime("%m%d-%H%M")+'.png', bbox_inches='tight')
+
